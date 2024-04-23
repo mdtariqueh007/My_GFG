cdb29e8024699ec0c3a016bc0dbf20ae5d727425
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-04-23 23:26:12 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Rohan's Love for Matrix/rohans-love-for-matrix.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+// User function Template for C++
+class Solution {
+  public:
+    int firstElement(int n) {
+        // code here
+        if(n==0 || n==1) return 1;
+        int a=1, b=1;
+        int mod= 1e9 + 7;
+        int c;
+        for(int i=0;i<n-2;i++){
+            c=(a+b)%mod;
+            a=b%mod;
+            b=c%mod;
+        }
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.firstElement(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
