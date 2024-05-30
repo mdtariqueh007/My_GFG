1affcbcff24de97d87efd355601c7ae8d9671726
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-05-31 00:13:55 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Swap two nibbles in a byte/swap-two-nibbles-in-a-byte.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int swapNibbles(int n) {
+        // code here
+        int x = n & 0x0f;
+        n = n>>4;
+        int y = n & 0x0f;
+        
+        int ans = x;
+        ans = ans<<4;
+        
+        return ans | y;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.swapNibbles(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
