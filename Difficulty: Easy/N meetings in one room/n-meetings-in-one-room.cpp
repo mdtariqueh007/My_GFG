fb04f05352210648fb6a903a3e48765cbf9d9362
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-08-04 00:08:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -2,14 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to find the maximum number of meetings that can
-    //be performed in a meeting room.
-    int maxMeetings(int start[], int end[], int n)
-    {
+class Solution {
+  public:
+    // Function to find the maximum number of meetings that can
+    // be performed in a meeting room.
+    int maxMeetings(int n, int start[], int end[]) {
         // Your code here
         vector<pair<int,int>> temp;
         
@@ -17,23 +16,21 @@ class Solution
             temp.push_back({start[i],end[i]});
         }
         
-        sort(temp.begin(),temp.end(),[&](pair<int,int>a,pair<int,int>b){
+        sort(temp.begin(),temp.end(),[&](pair<int,int> a,pair<int,int> b){
             return a.second<b.second;
         });
         
-        
         int cnt = 1;
-        int endTime = temp[0].second;
+        int last = temp[0].second;
         
         for(int i = 1;i<n;i++){
-            if(temp[i].first>endTime){
+            if(temp[i].first>last){
                 cnt++;
-                endTime = temp[i].second;
+                last = temp[i].second;
             }
         }
         
         return cnt;
-        
     }
 };
 
@@ -45,12 +42,14 @@ int main() {
         int n;
         cin >> n;
         int start[n], end[n];
-        for (int i = 0; i < n; i++) cin >> start[i];
+        for (int i = 0; i < n; i++)
+            cin >> start[i];
 
-        for (int i = 0; i < n; i++) cin >> end[i];
+        for (int i = 0; i < n; i++)
+            cin >> end[i];
 
         Solution ob;
-        int ans = ob.maxMeetings(start, end, n);
+        int ans = ob.maxMeetings(n, start, end);
         cout << ans << endl;
     }
     return 0;
