f4a222282a2d31d6bd27ad449d3a5cae4232af08
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-08-19 01:12:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -1,32 +1,29 @@
 //{ Driver Code Starts
-//Initial function template for C++
+// Initial function template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-//User function template for C++
 
-class Solution{
-    public:
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
     // arr : given array
-    // l : starting index of the array i.e 0
-    // r : ending index of the array i.e size-1
     // k : find kth smallest element and return using this function
-    int kthSmallest(int arr[], int l, int r, int k) {
-        //code here
-        int n = r + 1;
-        
+    int kthSmallest(vector<int> &arr, int k) {
+        // code here
         priority_queue<int> pq;
         
-        for(int i = 0;i<n;i++){
+        for(int x : arr){
             if(pq.size()<k){
-                pq.push(arr[i]);
+                pq.push(x);
             }
             else{
-                if(pq.top()>arr[i]){
+                if(pq.top()>x){
                     pq.pop();
-                    pq.push(arr[i]);
+                    pq.push(x);
                 }
             }
         }
@@ -36,25 +33,34 @@ class Solution{
 };
 
 //{ Driver Code Starts.
- 
-int main()
-{
+
+int main() {
     int test_case;
-    cin>>test_case;
-    while(test_case--)
-    {
-        int number_of_elements;
-        cin>>number_of_elements;
-        int a[number_of_elements];
-        
-        for(int i=0;i<number_of_elements;i++)
-            cin>>a[i];
-            
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
         int k;
-        cin>>k;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        k = crr[0];
+        int n = arr.size();
         Solution ob;
-        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+        cout << ob.kthSmallest(arr, k) << endl;
     }
     return 0;
 }
+
 // } Driver Code Ends
