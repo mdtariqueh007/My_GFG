e7d2df5b5fbfd9bab2e4aa59644e88ff3b2c66c3
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-09-15 10:39:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find element occuring once when all other are present thrice/find-element-occuring-once-when-all-other-are-present-thrice.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int singleElement(int arr[] ,int N) {
+        // code here
+        int ans = 0;
+        
+        for(int i = 0;i<32;i++){
+            int sum = 0;
+            
+            for(int j = 0;j<N;j++){
+                sum += (arr[j]>>i)&1;
+            }
+            
+            sum = sum%3;
+            
+            ans = ans | (sum<<i);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int arr[N];
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout<<ob.singleElement(arr,N);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
