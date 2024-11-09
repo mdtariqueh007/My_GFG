b4acd583ffb5e2a713cb423850bef40f16d641ca
Md Tarique Hussain <121678576+mdtariqueh007@users.noreply.github.com>
2024-11-10 00:47:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Two Sorted Arrays with Distinct Elements/union-of-two-sorted-arrays-with-distinct-elements.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // Your code here
+        // return vector with correct order of elements
+        
+        vector<int> ans;
+        
+        int i = 0, j = 0;
+        
+        int m = a.size(), n = b.size();
+        
+        while(i<m && j<n){
+            
+            if(a[i]==b[j]){
+                ans.push_back(a[i]);
+                i++;
+                j++;
+            }
+            else if(a[i]<b[j]){
+                ans.push_back(a[i]);
+                i++;
+            }
+            else{
+                ans.push_back(b[j]);
+                j++;
+            }
+                
+        }
+        
+        while(i<m){
+            ans.push_back(a[i]);
+            i++;
+        }
+        while(j<n){
+            ans.push_back(b[j]);
+            j++;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a, b;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // Read second array
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.findUnion(a, b);
+        for (int i : ans)
+            cout << i << ' ';
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
